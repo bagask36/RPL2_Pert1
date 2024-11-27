2bd9cf57f0e144847db63c7dc7a88ac861aaeae6
bagask36 <bagaskm36@gmail.com>
2024-11-27 11:26:13 +0700

    Menambahkan file proyek awal NetBeans

---
+++ src/rpl2_pert1/Component.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rpl2_pert1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Component implements Printable {
+    @Override
+    public void print() {
+        System.out.println("Bagas Keshena Maheswara");
+        System.out.println("4IA13");
+        System.out.println("50421255");
+    }
+}
---
+++ src/rpl2_pert1/Printable.java
@@ -0,0 +1,14 @@
+package rpl2_pert1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public interface Printable {
+    void print();
+}
